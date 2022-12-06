7078503b89768ed4178828890e34c777d388204b
Rayhanul Islam Shuvo <rayhanshuvo.bd@gmail.com>
2022-12-06 17:36:02 +0600

    manage git

--- src/pages/Dashboard/Admin/ManageBlog/index.jsx
+++ src/pages/Dashboard/Admin/ManageBlog/index.jsx
@@ -4,6 +4,7 @@ const ManageBlog = () => {
     return (
         <div>
             <h3> Mange blog...</h3>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste magni, quas assumenda nam odit incidunt cupiditate saepe itaque animi rem illo illum explicabo rerum nostrum eaque aperiam doloremque? Iusto aspernatur odit incidunt praesentium adipisci eius totam magnam, deserunt dolorum nisi illum, necessitatibus rerum? Exercitationem illum rerum culpa nihil blanditiis temporibus! </p>
         </div>
     );
 };
